5a0f31ae1a0734d17951c666728cbc715558eba0
EngMeow <mohamed.basyoni02@gmail.com>
2023-12-30 22:13:20 +0200

    Ds Cont

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/User/dashboard/dashboard.component';
+import { LoginComponent } from './Components/Auth/login/login.component';
+import { RegisterComponent } from './Components/Auth/register/register.component';
+import { ViewProfileComponent } from './Components/Profile/view-profile/view-profile.component';
+import { EditProfileComponent } from './Components/Profile/edit-profile/edit-profile.component';
+import { ApplicationsComponent } from './Components/User/applications/applications.component';
+import { JobsComponent } from './Components/User/jobs/jobs.component';
+import { NotfoundComponent } from './Components/Layout/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , redirectTo:'dashboard' , pathMatch:'full' },
+  {path:'dashboard' , component:DashboardComponent },
+  {path:'applications' , component:ApplicationsComponent },
+  {path:'jobs' , component:JobsComponent },
+  {path:'dashboard' , component:DashboardComponent },
+  {path:'login' , component:LoginComponent },
+  {path:'register' , component:RegisterComponent },
+  {path:':id/profile' , component:ViewProfileComponent },
+  {path:':id/profile/edit' , component:EditProfileComponent },
+  {path:'**' , component:NotfoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
